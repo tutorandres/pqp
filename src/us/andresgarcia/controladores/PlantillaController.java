27849022184c673508bb5f73a03a0621e4ff05cc
andres <andres@andres-PC>
2012-07-14 13:28:35 -0500

    cambios menores

---
+++ src/us/andresgarcia/controladores/PlantillaController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package us.andresgarcia.controladores;
+
+/**
+ *
+ * @author andres
+ */
+public class PlantillaController {
+    
+}
