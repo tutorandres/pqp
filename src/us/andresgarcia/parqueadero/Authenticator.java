7bd13d25781d5f23be15e52cddd12f7f85d01a8c
andres <andres@andres-PC>
2012-07-14 18:19:39 -0500

    cambios en plantilla, recuerde el volteo para eliminar un archivo del repositorio toca hacerlo por renombrar o copiarlo dos veces

---
+++ src/us/andresgarcia/parqueadero/Authenticator.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ */
+package us.andresgarcia.parqueadero;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Authenticator {
+    
+   private static final Map<String, String> USERS = new HashMap<String, String>();
+    
+    //static {
+        //USERS.put("demo", "demo");
+    //}
+    public static boolean validate(String user, String password){
+        String validUserPassword = USERS.get(user);
+        return validUserPassword != null && validUserPassword.equals(password);
+    }
+}
